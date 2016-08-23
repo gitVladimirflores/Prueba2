242ec1f29dfaecb5506c841500ae4a3a450ace5f
gitVladimirflores <vfloresupc@gmail.com>
2016-08-22 20:43:16 -0500

    Este es mi primer cambio

---
+++ demo1_flores/src/main/java/pe/edu/upc/demo1_flores/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demo1_flores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola mundo que tal" );
+    }
+}
